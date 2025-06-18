da025ec703cf73ae025cee0f44b8610929c3523b
FaizYanuar <fyigm23@gmail.com>
2025-06-18 16:37:44 +0700

    Initial Commit

---
+++ src/main/java/com/faiz/buku_resep_app/BukuResepAppApplication.java
@@ -0,0 +1,13 @@
+package com.faiz.buku_resep_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BukuResepAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BukuResepAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faiz/buku_resep_app/BukuResepAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faiz.buku_resep_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BukuResepAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
